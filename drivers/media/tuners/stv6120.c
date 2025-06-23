70a892b26d7f7a2c53653adb2bc2ca1c4e74358f
Deep Thought <deeptho@gmail.com>
2025-06-23 23:37:30 +0200

    Bug: TBS5927 and other cards based on stv6120 no longer working.

--- drivers/media/tuners/stv6120.c
+++ drivers/media/tuners/stv6120.c
@@ -28,6 +28,8 @@
 #include <asm/div64.h>
 
 #include "stv6120.h"
+#define dprintk(fmt, arg...)																					\
+	printk(KERN_DEBUG pr_fmt("%s:%d " fmt), __func__, __LINE__, ##arg)
 
 #define REG_N0		0
 #define REG_N1_F0	1
@@ -42,9 +44,9 @@ struct SLookup {
 	u16 RegValue;
 };
 
-static struct SLookup Gain_RFAGC_LookUp[] = {                
+static struct SLookup Gain_RFAGC_LookUp[] = {
 	/*Gain *100dB*/   /*reg*/
-	{	7429		,	0        },  /*	74.61 dB	*/
+	{	7429		,	0        },  /*	74.61 dB	weakest signal*/
 	{	7368		,	18711	 },  /*	74.43 dB	*/
 	{	7214		,	23432	 },  /*	73.36 dB	*/
 	{	7090		,	25123	 },  /*	72.4  dB	*/
@@ -100,7 +102,7 @@ static struct SLookup Gain_RFAGC_LookUp[] = {
 	{	2900		,	39226	 },  /*	22.63 dB	*/
 	{	2792		,	39520	 },  /*	21.62 dB	*/
 	{	2692		,	39792	 },  /*	20.62 dB	*/
-	{	2592    	,	40064	 },  /*	19.62 dB	*/
+	{	2592			,	40064	 },  /*	19.62 dB	*/
 	{	2497		,	40351	 },  /*	18.62 dB	*/
 	{	2392		,	40640	 },  /*	17.62 dB	*/
 	{	2290		,	40976	 },  /*	16.62 dB	*/
@@ -108,17 +110,17 @@ static struct SLookup Gain_RFAGC_LookUp[] = {
 	{	2088		,	41631	 },  /*	14.62 dB	*/
 	{	1999		,	41934	 },  /*	13.62 dB	*/
 	{	1875		,	42354	 },  /*	12.62 dB	*/
-	{	1764	   	,	42815	 },  /*	11.62 dB	*/
-	{	1637	   	,	43263	 },  /*	10.62 dB	*/	
-	{	1537	   	,	43743    },  /*	9.62  dB	*/		
-	{	1412		,	44288    },  /*	8.62  dB	*/	
-	{	1291		,	44913    },  /*	7.62  dB	*/		
-	{	1188	  	,	45712    },  /*	6.62  dB	*/	
-	{	1080    	,	46720    },  /*	5.63  dB	*/	
-	{	976		,       48164	 },  /*	4.63  dB	*/	
-	{	930	    	,	50816    },  /*	3.63  dB	*/	
-	{	898	    	,	65534    },  /*	2.94  dB	*/	
-	{	880	    	,	65535    }  /*	2.95  dB	*/	
+	{	1764			,	42815	 },  /*	11.62 dB	*/
+	{	1637			,	43263	 },  /*	10.62 dB	*/
+	{	1537			,	43743    },  /*	9.62  dB	*/
+	{	1412		,	44288    },  /*	8.62  dB	*/
+	{	1291		,	44913    },  /*	7.62  dB	*/
+	{	1188			,	45712    },  /*	6.62  dB	*/
+	{	1080			,	46720    },  /*	5.63  dB	*/
+	{	976		,       48164	 },  /*	4.63  dB	*/
+	{	930				,	50816    },  /*	3.63  dB	*/
+	{	898				,	65534    },  /*	2.94  dB	*/
+	{	880				,	65535    }  /*	2.95  dB	*/
 };
 
 #ifndef ARRAY_SIZE
@@ -168,16 +170,16 @@ struct stv {
 
 	struct stv6120_cfg *cfg;
 
-	u8 reg[7];
+	u8 reg[7]; //registers starting at reg[0]=CTRL3
 };
 
 static int i2c_read(struct i2c_adapter *adap,
-		    u8 adr, u8 *msg, int len, u8 *answ, int alen)
+				u8 adr, u8 *msg, int len, u8 *answ, int alen)
 {
 	struct i2c_msg msgs[2] = { { .addr = adr, .flags = 0,
-				     .buf = msg, .len = len},
-				   { .addr = adr, .flags = I2C_M_RD,
-				     .buf = answ, .len = alen } };
+						 .buf = msg, .len = len},
+					 { .addr = adr, .flags = I2C_M_RD,
+						 .buf = answ, .len = alen } };
 	if (i2c_transfer(adap, msgs, 2) != 2) {
 		pr_err("stv6120: i2c_read error\n");
 		return -1;
@@ -188,7 +190,7 @@ static int i2c_read(struct i2c_adapter *adap,
 static int i2c_write(struct i2c_adapter *adap, u8 adr, u8 *data, int len)
 {
 	struct i2c_msg msg = {.addr = adr, .flags = 0,
-			      .buf = data, .len = len};
+						.buf = data, .len = len};
 
 	if (i2c_transfer(adap, &msg, 1) != 1) {
 		pr_err("stv6120: i2c_write error\n");
@@ -196,19 +198,23 @@ static int i2c_write(struct i2c_adapter *adap, u8 adr, u8 *data, int len)
 	}
 	return 0;
 }
-#if 0
+
+#ifdef UNUSED
 static int write_regs(struct stv *state, int reg, int len)
 {
 	u8 d[8];
 
-	u8 base = 0x02 + 0xa * state->nr;
+	//u8 base = 0x02 + 0xa * state->nr;
 
-	memcpy(&d[1], &state->base->reg[reg], len);
+	memcpy(&d[1], &state->reg[reg], len);
 	d[0] = reg;
 	return i2c_write(state->base->i2c, state->base->adr, d, len + 1);
 }
 #endif
 
+/*
+	write CTRL3 ... CTRL10
+ */
 static int write_tuner_regs(struct stv *state)
 {
 	u8 d[8];
@@ -282,11 +288,11 @@ static int probe(struct stv *state)
 	case 1:
 		d[10] = 0xFA; /* RF C */
 		d[11] = 0x13; /* LNAC ON */
-		break;	    
+		break;
 	case 2:
 		d[10] = 0xF5; /* RF B */
 		d[11] = 0x0B; /* LNAB ON */
-		break;	    
+		break;
 	}
 	d[0] = 0;
 	ret = i2c_write(state->base->i2c, state->base->adr, d, 25 + 1);
@@ -333,6 +339,7 @@ static void release(struct dvb_frontend *fe)
 	fe->tuner_priv = NULL;
 }
 
+
 #if 0
 static int set_bandwidth(struct dvb_frontend *fe, u32 CutOffFrequency)
 {
@@ -343,11 +350,11 @@ static int set_bandwidth(struct dvb_frontend *fe, u32 CutOffFrequency)
 		index = 6;
 	if (index > 50)
 		index = 50;
-	if ((state->base->reg[0x08] & ~0xFC) == ((index-6) << 2))
+	if ((state->reg[0x08] & ~0xFC) == ((index-6) << 2))
 		return 0;
 
-	state->base->reg[0x08] = (state->base->reg[0x08] & ~0xFC) | ((index-6) << 2);
-	state->base->reg[0x09] = (state->base->reg[0x09] & ~0x0C) | 0x08;
+	state->reg[0x08] = (state->reg[0x08] & ~0xFC) | ((index-6) << 2);
+	state->reg[0x09] = (state->reg[0x09] & ~0x0C) | 0x08;
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 1);
 	write_regs(state, 0x08, 2);
@@ -356,7 +363,6 @@ static int set_bandwidth(struct dvb_frontend *fe, u32 CutOffFrequency)
 		fe->ops.i2c_gate_ctrl(fe, 0);
 	return 0;
 }
-
 #endif
 
 static int set_lof(struct stv *state, u32 LocalFrequency, u32 CutOffFrequency)
@@ -364,11 +370,10 @@ static int set_lof(struct stv *state, u32 LocalFrequency, u32 CutOffFrequency)
 	int cf_index = (CutOffFrequency / 1000000) - 5;
 	u32 Frequency = (LocalFrequency + 500) / 1000; // Hz -> kHz
 	u32 fvco, div, frac;
-	u8 Icp, tmp;
+	u8 Icp;
 
 	u8 PDIV, P;
 
-	//pr_info("F = %u, CutOff = %u\n", Frequency, CutOffFrequency);
 
 
 	/* set PDIV and CF */
@@ -376,7 +381,10 @@ static int set_lof(struct stv *state, u32 LocalFrequency, u32 CutOffFrequency)
 		cf_index = 0;
 	if (cf_index > 31)
 		cf_index = 31;
-
+#if 0
+	dprintk("F = %u, CutOff = %u => %uMhz cf_index=%d\n", Frequency, CutOffFrequency,
+					cutoff_from_index(cf_index),  cf_index);
+#endif
 	if (Frequency >= 1191000) {
 		PDIV = 0;
 		P    = 2;
@@ -415,24 +423,43 @@ static int set_lof(struct stv *state, u32 LocalFrequency, u32 CutOffFrequency)
 	frac = (fvco * state->cfg->Rdiv) % state->cfg->xtal;
 	frac = MulDiv32(frac, 0x40000, state->cfg->xtal);
 
-
+	/*START set carrier freq*/
+	//addr 0x2+0 = CTRL3
 	state->reg[REG_N0]    = div & 0xff;
+
+	//addr 0x2+1 = CTRL4
 	state->reg[REG_N1_F0] = (((div >> 8) & 0x01) | ((frac & 0x7f) << 1)) & 0xff;
+
+	//addr 0x2+2 = CTRL5
 	state->reg[REG_F1]    = (frac >> 7) & 0xff;
+
+	//addr 0x2+3 = CTRL6
 	state->reg[REG_F2_ICP] &= 0x88;
 	state->reg[REG_F2_ICP] |= (Icp << 4) | ((frac >> 15) & 0x07);
-	state->reg[REG_CF_PDIV] &= 0x9f;
-	state->reg[REG_CF_PDIV] |= ((PDIV << 5) | cf_index);
 
-	/* Start cal vco,CF */
-	state->reg[REG_CAL] &= 0xf8;
-	state->reg[REG_CAL] |= 0x06;
+	/*Last part of set carrier freq + bandwidth selection*/
+	//addr 0x2+4 = CTRL7
 
-	write_tuner_regs(state);
+	{
+		u8 newval = state->reg[REG_CF_PDIV];
+		newval &= 0x9f;
+		newval |= ((PDIV << 5) | cf_index);
+		if(newval != 	state->reg[REG_CF_PDIV]) {
+			state->reg[REG_CF_PDIV] = newval;
+			/* Start cal vco,CF */
+			//addr 0x2+6 =  STAT1
+			state->reg[REG_CAL] &= 0xf8;
+			state->reg[REG_CAL] |= 0x06;//bit[1] starts calibration
 
-	wait_for_call_done(state, 0x06);
+			write_tuner_regs(state);
+			wait_for_call_done(state, 0x06);
 
-	usleep_range(10000, 12000);
+			usleep_range(10000, 12000);
+		} else {
+			write_tuner_regs(state);
+			wait_for_call_done(state, 0x06);
+		}
+	}
 
 #if 0
 	read_reg(state, 0x03, &tmp);
@@ -451,11 +478,10 @@ static int set_params(struct dvb_frontend *fe)
 {
 	struct stv *state = fe->tuner_priv;
 	struct dtv_frontend_properties *p = &fe->dtv_property_cache;
-	int status;
-	u32 freq, symb, cutoff;
+	u32 freq, symbol_rate, cutoff;
 	u32 rolloff;
 
-	if (p->delivery_system != SYS_DVBS && p->delivery_system != SYS_DVBS2)
+	if (p->delivery_system != SYS_DVBS && p->delivery_system != SYS_DVBS2 && p->delivery_system != SYS_AUTO)
 		return -EINVAL;
 
 	switch (p->rolloff) {
@@ -471,16 +497,35 @@ static int set_params(struct dvb_frontend *fe)
 	}
 
 	freq = p->frequency * 1000;
-	symb = p->symbol_rate;
-	cutoff = 5000000 + MulDiv32(p->symbol_rate, rolloff, 200);
+
+	symbol_rate = p->symbol_rate;
+
+	cutoff = 5000000 + MulDiv32(symbol_rate, rolloff, 200);
 
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 1);
 	set_lof(state, freq, cutoff);
 	if (fe->ops.i2c_gate_ctrl)
 		fe->ops.i2c_gate_ctrl(fe, 0);
-	return status;
+	return 0;
 }
+
+
+/*
+	frequency in kHz, bandwidth in kHz
+ */
+static int set_frequency_and_bandwidth(struct dvb_frontend *fe, u32 frequency, u32 bandwidth)
+{
+	struct stv *state = fe->tuner_priv;
+
+	if (fe->ops.i2c_gate_ctrl)
+		fe->ops.i2c_gate_ctrl(fe, 1);
+	set_lof(state, frequency*1000, bandwidth*1000);
+	if(fe->ops.i2c_gate_ctrl)
+		fe->ops.i2c_gate_ctrl(fe, 0);
+	return 0;
+}
+
 
 static s32 TableLookup(struct SLookup *Table, int TableSize, u16 RegValue)
 {
@@ -489,8 +534,8 @@ static s32 TableLookup(struct SLookup *Table, int TableSize, u16 RegValue)
 	int imin = 0;
 	int imax = TableSize - 1;
 	int i;
-	
-	// Assumes Table[0].RegValue < Table[imax].RegValue 
+
+	// Assumes Table[0].RegValue < Table[imax].RegValue
 	if( RegValue <= Table[0].RegValue )
 		Gain = Table[0].Value;
 	else if( RegValue >= Table[imax].RegValue )
@@ -499,7 +544,7 @@ static s32 TableLookup(struct SLookup *Table, int TableSize, u16 RegValue)
 		while(imax-imin > 1) {
 			i = (imax + imin) / 2;
 			if ((Table[imin].RegValue <= RegValue) &&
-			    (RegValue <= Table[i].RegValue) )
+					(RegValue <= Table[i].RegValue) )
 				imax = i;
 			else
 				imin = i;
@@ -508,31 +553,36 @@ static s32 TableLookup(struct SLookup *Table, int TableSize, u16 RegValue)
 		Gain = Table[imin].Value;
 		if (RegDiff != 0)
 			Gain += ((s32) (RegValue - Table[imin].RegValue) *
-			    (s32)(Table[imax].Value - Table[imin].Value))/(RegDiff);
+					(s32)(Table[imax].Value - Table[imin].Value))/(RegDiff);
 	}
 	return Gain;
 }
 
-static int get_rf_strength(struct dvb_frontend *fe, u16 *agc)
+
+static int agc_to_gain_dbm(struct dvb_frontend *fe, s32 agc)
 {
 	struct dtv_frontend_properties *p = &fe->dtv_property_cache;
-
+	s32 bbgain = 2*tuner_init[1]&0xf; //CTRL2; value is 6
 	s32 gain = 1, ref_bbgain = 12, tilt = 6;
 	s32 freq;
 
-	gain = TableLookup(Gain_RFAGC_LookUp, ARRAY_SIZE(Gain_RFAGC_LookUp), *agc);
+	gain = TableLookup(Gain_RFAGC_LookUp, ARRAY_SIZE(Gain_RFAGC_LookUp), agc);
 
-	gain += 100 * (6 - ref_bbgain); 	
+	gain += 100 * (bbgain - ref_bbgain);
 
 	freq = p->frequency / 10000;
 
 	if (freq<159)
-		gain -= 200; /* HMR filter 2dB gain compensation below freq=1590MHz */	
-	
-	gain -= (((freq-155)*tilt)/12)*10;
-	
-	*agc = gain;
+		gain -= 200; /* HMR filter 2dB gain compensation below freq=1590MHz */
 
+	gain -= (((freq-155)*tilt)/12)*10;
+
+
+	return gain;
+}
+static int get_rf_strength(struct dvb_frontend *fe, u16 *agc)
+{
+	*agc = agc_to_gain_dbm(fe, *agc);
 	return 0;
 }
 
@@ -547,6 +597,12 @@ static struct dvb_tuner_ops tuner_ops = {
 	.set_params        = set_params,
 	.release           = release,
 	.get_rf_strength   = get_rf_strength,
+	.agc_to_gain_dbm   = agc_to_gain_dbm,
+	.set_frequency_and_bandwidth = set_frequency_and_bandwidth
+#if 0
+	.set_bandwidth     = set_bandwidth,
+	.set_frequency     = set_frequency,
+#endif
 };
 
 static struct stv_base *match_base(struct i2c_adapter  *i2c, u8 adr)
@@ -560,7 +616,7 @@ static struct stv_base *match_base(struct i2c_adapter  *i2c, u8 adr)
 }
 
 struct dvb_frontend *stv6120_attach(struct dvb_frontend *fe,
-		    struct i2c_adapter *i2c, struct stv6120_cfg *cfg, int nr)
+				struct i2c_adapter *i2c, struct stv6120_cfg *cfg, int nr)
 {
 	struct stv *state;
 	struct stv_base *base;
@@ -613,4 +669,3 @@ EXPORT_SYMBOL_GPL(stv6120_attach);
 MODULE_DESCRIPTION("STV6120 driver");
 MODULE_AUTHOR("Luis Alves");
 MODULE_LICENSE("GPL");
-
